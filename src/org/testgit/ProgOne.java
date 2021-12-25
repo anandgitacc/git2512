3422d227c8cc8020b5f2adc70282e2b67d3af83e
anandgitacc <anand.akmail@gmail.com>
2021-12-25 16:36:20 +0530

    firstgit

---
+++ src/org/testgit/ProgOne.java
@@ -0,0 +1,20 @@
+package org.testgit;
+
+public class ProgOne {
+	
+	private void empName() {
+		System.out.println("Anand");
+	}
+	
+	private void empMobile() {
+		System.out.println("8939278836");
+	}
+	
+	public static void main(String[] args) {
+		
+		ProgOne e = new ProgOne();
+		e.empMobile();
+		e.empName();
+	}
+
+}

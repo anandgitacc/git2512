d17bc36815b4e98d65b5cdd8940dde9c0c4def9c
anandgitacc <anand.akmail@gmail.com>
2021-12-25 16:50:28 +0530

    anandgitempid

--- src/org/testgit/ProgOne.java
+++ src/org/testgit/ProgOne.java
@@ -10,6 +10,11 @@ public class ProgOne {
 		System.out.println("8939278836");
 	}
 	
+	private void empId() {
+		
+		System.out.println("Employee id : 123");
+	}
+	
 	public static void main(String[] args) {
 		
 		ProgOne e = new ProgOne();
